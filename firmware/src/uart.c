17e863521cb2e5e2ad4387446ef54931121d24da
Menci <mencici@msn.com>
2024-09-28 17:56:46 +0800

    handle touch protocol

--- firmware/src/uart.c
+++ firmware/src/uart.c
@@ -8,6 +8,7 @@
 #include "hardware/gpio.h"
 #include "hardware/uart.h"
 #include "tusb.h"
+#include <stdarg.h>
 
 #include "board_defs.h"
 
@@ -15,159 +16,193 @@
 #define UART_DATABITS 8
 #define UART_STOPBITS 1
 
-#define UART_BUFFER_SIZE 1024
+#define TOUCH_BUFFER_SIZE 16
 
-inline static uint32_t min(uint32_t a, uint32_t b)
+typedef struct {
+    bool left_brace; // "{"/"(" has been read
+    char buffer[TOUCH_BUFFER_SIZE + 1];
+    size_t i;
+} touch_buffer_t;
+
+static void cdprintf(const char *format, ...)
 {
-    return a < b ? a : b;
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+    tud_cdc_n_write(0, buffer, strlen(buffer));
 }
 
-typedef struct
+static bool touch_buffer_on_data(touch_buffer_t *buffer, uint8_t data)
 {
-    uint8_t *begin, *end, *head;
-    size_t capacity;
-    size_t count;
-} ring_buffer_t;
+    if (buffer->left_brace)
+    {
+        if (data == '}' || data == ')')
+        {
+            // Null-terminate buffer
+            buffer->buffer[buffer->i] = 0;
+            // Reset buffer
+            buffer->i = 0;
+            buffer->left_brace = false;
+            return true;
+        }
 
-inline void ring_init(ring_buffer_t *buffer, void *data, size_t size)
-{
-    buffer->begin = data;
-    buffer->end = data + size;
-    buffer->head = data;
-    buffer->capacity = size;
-    buffer->count = 0;
-}
-
-inline void ring_clear(ring_buffer_t *buffer)
-{
-    buffer->head = buffer->begin;
-    buffer->count = 0;
-}
-
-inline uint32_t ring_can_pop(ring_buffer_t *buffer)
-{
-    return buffer->count;
-}
-
-inline uint8_t ring_pop(ring_buffer_t *buffer)
-{
-    uint8_t data = *buffer->head;
-    buffer->head++;
-
-    if (buffer->head == buffer->end)
-        buffer->head = buffer->begin;
-    buffer->count--;
-
-    return data;
-}
-
-inline uint32_t ring_can_push(ring_buffer_t *buffer)
-{
-    return buffer->capacity - buffer->count;
-}
-
-inline void ring_push(ring_buffer_t *buffer, uint8_t data)
-{
-    uint8_t *tail = buffer->head + buffer->count;
-    if (tail >= buffer->end)
-        tail -= buffer->capacity;
-    *tail = data;
-    buffer->count++;
-}
-
-ring_buffer_t buffer_u2t, buffer_t2u;
-
-static void tick_tud_read(uint8_t itf)
-{
-    uint32_t len = min(tud_cdc_n_available(itf), ring_can_push(&buffer_t2u));
-    if (len) {
-        uint8_t data[len];
-        uint32_t real_len = tud_cdc_n_read(itf, data, len);
-        for (uint32_t i = 0; i < real_len; i++)
-            ring_push(&buffer_t2u, data[i]);
+        if (buffer->i < TOUCH_BUFFER_SIZE)
+        {
+            buffer->buffer[buffer->i++] = data;
+        }
     }
-}
-
-static void tick_tud_write(uint8_t itf)
-{
-    uint32_t len = min(tud_cdc_n_write_available(itf), ring_can_pop(&buffer_u2t));
-    if (len) {
-        uint8_t data[len];
-        for (uint32_t i = 0; i < len; i++)
-            data[i] = ring_pop(&buffer_u2t);
-        tud_cdc_n_write(itf, data, len);
-        tud_cdc_n_write_flush(itf);
+    else
+    {
+        if (data == '{' || data == '(')
+        {
+            buffer->left_brace = true;
+        }
     }
-}
-
-static void tick_uart_read()
-{
-    while (ring_can_push(&buffer_u2t)) {
-        if (!uart_is_readable(UART_PORT))
-            break;
-        ring_push(&buffer_u2t, uart_getc(UART_PORT));
-    }
-}
-
-static void tick_uart_write()
-{
-    while (ring_can_pop(&buffer_t2u)) {
-        if (!uart_is_writable(UART_PORT))
-            break;
-        uart_putc_raw(UART_PORT, ring_pop(&buffer_t2u));
-    }
+    return false;
 }
 
 void io_uart_init(uint8_t tx_pin, uint8_t rx_pin)
 {
-    gpio_set_function(rx_pin, GPIO_FUNC_UART);
     gpio_set_function(tx_pin, GPIO_FUNC_UART);
+    gpio_set_function(rx_pin, GPIO_FUNC_UART);
+    gpio_set_pulls(tx_pin, 1, 0);
+    gpio_set_pulls(rx_pin, 1, 0);
 
-    static uint8_t buffer_u2t_data[UART_BUFFER_SIZE], buffer_t2u_data[UART_BUFFER_SIZE];
-    ring_init(&buffer_u2t, buffer_u2t_data, UART_BUFFER_SIZE);
-    ring_init(&buffer_t2u, buffer_t2u_data, UART_BUFFER_SIZE);
+    uart_init(UART_PORT, UART_BAUDRATE);
+    uart_set_hw_flow(UART_PORT, false, false);
+    uart_set_format(UART_PORT, UART_DATABITS, UART_STOPBITS, UART_PARITY_NONE);
+    uart_set_fifo_enabled(UART_PORT, true);
 }
 
-static void uart_check_connected(uint8_t itf, bool* just_connected, bool* just_disconnected)
+#define TOUCH_DATA_LENGTH 7
+struct {
+    char touch_data[TOUCH_DATA_LENGTH];
+    bool has_touch_data;
+    bool conditioning_mode;
+} touch_state;
+
+static void handle_u2t(uint8_t itf, char *buffer)
 {
-    static bool was_connected;
-    bool connected = tud_cdc_n_connected(itf);
-    if (was_connected && !connected) {
-        *just_connected = false;
-        *just_disconnected = true;
-    } else if (!was_connected && connected) {
-        *just_connected = true;
-        *just_disconnected = false;
-    } else {
-        *just_connected = false;
-        *just_disconnected = false;
+    size_t length = strlen(buffer);
+    if (length == 4) // "LAr2"
+    {
+        cdprintf("handle_u2t(): pass-through 4-byte response: %s\r\n", buffer);
+
+        char write_buffer[] = "(LAr2)";
+        memcpy(write_buffer + 1, buffer, 4);
+        // if (tud_cdc_n_connected(itf))
+        {
+            tud_cdc_n_write(itf, write_buffer, strlen(write_buffer));
+            tud_cdc_n_write_flush(itf);
+        }
     }
-    was_connected = connected;
+    else if (length == 7) // "......."
+    {
+        if (touch_state.conditioning_mode)
+        {
+            cdprintf("handle_u2t(): ignored touch data %s in conditioning mode\r\n", buffer);
+            // ignore touch data in conditioning mode
+        }
+        else
+        {
+            if (!touch_state.has_touch_data ||
+                memcmp(touch_state.touch_data, buffer, TOUCH_DATA_LENGTH) != 0)
+            {
+                memcpy(touch_state.touch_data, buffer, TOUCH_DATA_LENGTH);
+                touch_state.has_touch_data = true;
+
+                cdprintf("handle_u2t(): updating touch data: %s\r\n", buffer);
+            }
+
+            char write_buffer[] = "(.......)";
+            memcpy(write_buffer + 1, buffer, TOUCH_DATA_LENGTH);
+            // if (tud_cdc_n_connected(itf))
+            {
+                tud_cdc_n_write(itf, write_buffer, strlen(write_buffer));
+                tud_cdc_n_write_flush(itf);
+            }
+        }
+    }
+}
+
+static void handle_t2u(uint8_t itf, char *buffer)
+{
+    if (strcmp(buffer, "RSET") == 0)
+    {
+        touch_state.has_touch_data = false;
+        touch_state.conditioning_mode = false;
+    }
+    else if (strcmp(buffer, "HALT") == 0)
+    {
+        touch_state.has_touch_data = false;
+        touch_state.conditioning_mode = true;
+    }
+    else if (strcmp(buffer, "STAT") == 0)
+    {
+        touch_state.conditioning_mode = false;
+    }
+    else if (touch_state.conditioning_mode &&
+             strlen(buffer) == 4 &&
+             (buffer[0] == 'L' || buffer[0] == 'R') && (buffer[2] == 'r' || buffer[2] == 'k'))
+    {
+        // conditioning mode command
+    }
+    else
+    {
+        cdprintf("handle_t2u(): Unknown command: %s\r\n", buffer);
+        return;
+    }
+
+    cdprintf("handle_t2u(): pass-through command: %s\r\n", buffer);
+
+    // Passthrough the command
+    uart_putc_raw(UART_PORT, '{');
+    for (size_t i = 0; i < 4; i++)
+    {
+        uart_putc_raw(UART_PORT, buffer[i]);
+    }
+    uart_putc_raw(UART_PORT, '}');
 }
 
 void io_uart_run(uint8_t itf)
 {
-    bool just_connected, just_disconnected;
-    uart_check_connected(itf, &just_connected, &just_disconnected);
-    if (just_connected) {
-        ring_clear(&buffer_u2t);
-        ring_clear(&buffer_t2u);
-
-        uart_init(UART_PORT, UART_BAUDRATE);
-        uart_set_hw_flow(UART_PORT, false, false);
-        uart_set_format(UART_PORT, UART_DATABITS, UART_STOPBITS, UART_PARITY_NONE);
-        uart_set_fifo_enabled(UART_PORT, true);
-    } else if (just_disconnected) {
-        uart_deinit(UART_PORT);
+    static touch_buffer_t u2t, t2u;
+    while (uart_is_readable(UART_PORT))
+    {
+        if (touch_buffer_on_data(&u2t, uart_getc(UART_PORT)))
+        {
+            handle_u2t(itf, u2t.buffer);
+        }
     }
-    tick_tud_read(itf);
-    tick_uart_read();
-    tick_tud_write(itf);
-    tick_uart_write();
 
-    if (tud_cdc_n_connected(0)) {
-        static char buffer[1024];
-        sprintf(buffer, "U2T buffer size: %u, T2U buffer size: %u\r\n", buffer_u2t.count, buffer_t2u.count);
-        tud_cdc_n_write(0, buffer, strlen(buffer));
+    static bool connected = false;
+    if (tud_cdc_n_connected(itf) && !connected)
+    {
+        connected = true;
+        cdprintf("io_uart_run(): connected to cdc\r\n");
+    }
+    else if (!tud_cdc_n_connected(itf) && connected)
+    {
+        connected = false;
+        cdprintf("io_uart_run(): disconnected from cdc\r\n");
+    }
+
+    if (tud_cdc_n_available(itf))
+    {
+        while (true)
+        {
+            int32_t ch = tud_cdc_n_read_char(itf);
+            if (ch < 0)
+            {
+                break;
+            }
+            cdprintf("io_uart_run(): from cdc: %c\r\n", ch);
+            if (touch_buffer_on_data(&t2u, ch))
+            {
+                handle_t2u(itf, t2u.buffer);
+            }
+        }
     }
 }
